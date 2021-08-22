1bbbcfacfe4dc9bf3c189e8061111a511753143d
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-22 03:07:52 -0400

    modify order details page to contain more details of the ordered items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @line_items = @order.line_items
+    @line_item_products = Product.where(id: @line_items.map { |item| item.product_id })
   end
 
   def create
