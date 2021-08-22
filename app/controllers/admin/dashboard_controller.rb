742986fa4cc29240ee3ab87a04186ecc0976ec72
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-22 04:09:23 -0400

    modify to show stats about the store; products & categories count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,8 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
 
   def show
-    @product_count = Product.all.size
-    @category_count = Category.all.size
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
 end
