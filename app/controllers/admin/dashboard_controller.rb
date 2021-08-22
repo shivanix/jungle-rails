31e0dd84b1a19bcf18c5f72a8169b408e1aa227e
shivanix <xuxu@LAPTOP-9J0KT8BE.localdomain>
2021-08-22 03:52:08 -0400

    add HTTP Basic Auth to all admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: "Jungle", password: "book"
+
   def show
+    @product_count = Product.all.size
+    @category_count = Category.all.size
   end
+
 end
